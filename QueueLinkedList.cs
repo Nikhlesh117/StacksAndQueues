42c62ee6885ff3efc4c7cff24f0ce94cae2737a4
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-23 11:21:04 +0530

    Added: Dequeue

---
+++ QueueLinkedList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    public class QueueLinkedList
+    {
+        public Node head = null ;
+
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+
+
+            }
+            Console.WriteLine("{0} Pushed into stack", node.data);
+        }
+
+        public void Dequeue()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            Node temp = this.head;
+            this.head = this.head.next;
+
+            Console.WriteLine("\nItem Dequeue is {0}", temp.data);
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
