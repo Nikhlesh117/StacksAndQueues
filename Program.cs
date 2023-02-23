84791e1cd1daed811ca248ad11f0c83154e05da2
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-23 10:52:30 +0530

    Added: Peak And Pop

--- Program.cs
+++ Program.cs
@@ -1,10 +1,31 @@
-﻿namespace StacksAndQueues
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Stacks And Queues");
+            Console.WriteLine("----------------------------");
+
+            StackLinkedList stack= new StackLinkedList();
+
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+
+            stack.Peak();
+            stack.Pop();
+
+
+            stack.IsEmpty();
+            stack.Display();
+
         }
     }
 }
---
+++ StackLinkedList.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    public class StackLinkedList
+    {
+        public Node top;
+
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.top == null)
+            {
+                this.top = node;
+            }
+            else
+            {
+                node.next = this.top;
+                this.top = node;
+
+                
+            }
+            Console.WriteLine("{0} Pushed into stack", node.data);
+        }
+
+        public void Peak()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the Peak of the stack  ", this.top.data);
+        }
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty, Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("Value popped is {0} ", this.top.data);
+            this.top = this.top.next;
+        }
+        public void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peak();
+                Pop();
+            }
+        }
+
+
+
+
+
+        public void Display()
+        {
+            Node temp = this.top;
+            if (temp == null)
+            {
+                Console.WriteLine("stack is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
