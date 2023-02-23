ba15434f0869ffb8119b3a03a2c22d36806eae90
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-23 11:03:58 +0530

    Added: Create Queue

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace StacksAndQueues
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Stacks And Queues");
+            Console.WriteLine("----------------------------");
+
+
+            QueueLinkedList queue = new QueueLinkedList();
+
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
         }
     }
 }
---
+++ QueueLinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    public class QueueLinkedList
+    {
+        public Node head = null ;
+
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+
+
+            }
+            Console.WriteLine("{0} Pushed into stack", node.data);
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
