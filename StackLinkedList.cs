0f633bd89f412ac0fd490981e390fc567a42df2e
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-23 10:40:06 +0530

    Added: Create Stack

---
+++ StackLinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    public class StackLinkedList
+    {
+        public Node top;
+
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.top == null)
+            {
+                this.top = node;
+            }
+            else
+            {
+                node.next = this.top;
+                this.top = node;
+
+                
+            }
+            Console.WriteLine("{0} Pushed into stack", node.data);
+        }
+
+        public void Display()
+        {
+            Node temp = this.top;
+            if (temp == null)
+            {
+                Console.WriteLine("stack is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
